2fe64db417f18f6842f76c07437ca3394fb9c1cd
Maxim <maxim.k.elasnce@gmail.com>
2026-01-29 21:47:28 +0300

    first commit

---
+++ acc/README.md
@@ -0,0 +1,6 @@
+# Test for a job
+
+This is test for job.
+
+Use cmake to build.
+Recommended standard is 20/23
---
+++ acc/acc.cpp
@@ -0,0 +1,12 @@
+﻿// acc.cpp: определяет точку входа для приложения.
+//
+
+#include "acc.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
