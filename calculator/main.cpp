84374eb3c062eb1350dd7fd7813ff566d29d3df8
Sub <maxim.k.elasnce@gmail.com>
2026-01-30 16:51:04 +0300

    Read of --cfg command line parameter

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -3,10 +3,25 @@
 
 int main(int argc, char *argv[]) {
     namespace po = boost::program_options;
+
+    std::string configPath;
+
     po::options_description desc("Options of the calculator");
     desc.add_options()
-        ("-cfg", "produce help message")
-        ("compression", po::value<int>(), "set compression level")
-        ;
+    ("config,cfg", po::value<std::string>(&configPath), "config file path");
+
+    po::variables_map vm;
+    try {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+    }
+    catch(po::unknown_option& ex)
+    {
+        std::cout << "Bad options" << "\n";
+        exit(1);
+    }
+    po::notify(vm);
+    if (vm.count("config")) {
+        std::cout << "Config: " << configPath << "\n";
+    }
     return 0;
 }
