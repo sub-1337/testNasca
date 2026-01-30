186f6fc7f469eefae1c76e9a1a6c763de2ca82ae
Sub <maxim.k.elasnce@gmail.com>
2026-01-30 16:00:39 +0300

    local boost

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,11 +1,12 @@
-#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 #include <iostream>
 
-int main() {
-    boost::filesystem::path p("C:/");
-    std::cout << "Files in C:/:" << std::endl;
-    for (auto& entry : boost::filesystem::directory_iterator(p)) {
-        std::cout << entry.path().string() << std::endl;
-    }
+int main(int argc, char *argv[]) {
+    namespace po = boost::program_options;
+    po::options_description desc("Options of the calculator");
+    desc.add_options()
+        ("-cfg", "produce help message")
+        ("compression", po::value<int>(), "set compression level")
+        ;
     return 0;
 }
