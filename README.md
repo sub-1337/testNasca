16e1a0f7e63ce442cbce02491422a0111157e3e3
Sub <maxim.k.elasnce@gmail.com>
2026-01-30 15:09:10 +0300

    fixed boost

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # testNasca
 
+Building:
+1) Go to `clculator`
+2) Change `CMakeLists.txt` to yours boost path
+3) Cmake folder
+4) Run
--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,9 +1,11 @@
+#include <boost/filesystem.hpp>
 #include <iostream>
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello World lol" << endl;
+int main() {
+    boost::filesystem::path p("C:/");
+    std::cout << "Files in C:/:" << std::endl;
+    for (auto& entry : boost::filesystem::directory_iterator(p)) {
+        std::cout << entry.path().string() << std::endl;
+    }
     return 0;
 }
