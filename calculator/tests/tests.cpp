700ce69184db4f8ec9cb7e87c7b25c84f4f65bf3
Sub <maxim.k.elasnce@gmail.com>
2026-01-31 15:57:17 +0300

    added creation of folder test

--- calculator/tests/tests.cpp
+++ calculator/tests/tests.cpp
@@ -1,6 +1,6 @@
 #include "../application_config.h"
 #include <catch2/catch_test_macros.hpp>
-
+#include <filesystem>
 TEST_CASE("Test app config", "[core]")
 {
     application_config config("config_1.toml");
@@ -19,11 +19,14 @@ TEST_CASE("Test app config 2", "[core]")
 
 TEST_CASE("Test app config 3", "[core]")
 {
+    namespace fs = std::filesystem;
+    fs::remove("output");
     application_config config("config_3.toml");
     REQUIRE(config.get_input() == "input_dir" );
     REQUIRE(config.get_output() == "output" );
     REQUIRE(config.get_filename_mask().size() == 1);
     REQUIRE(config.get_filename_mask()[0] == "trade");
+    REQUIRE((fs::exists("output") && fs::is_directory("output")) == true);
 }
 
 TEST_CASE("Test app config 4", "[core]")
